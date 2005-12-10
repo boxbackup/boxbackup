ed37c178ee05181410969e45ada54096071ab4eb
Chris Wilson <chris+github@qwirx.com>
2005-12-10 15:58:03 +0000

    * ServiceBackupDaemon.cpp - Moved Winsock initialisation to bbackupd.cpp

--- bin/bbackupd/ServiceBackupDaemon.cpp
+++ bin/bbackupd/ServiceBackupDaemon.cpp
@@ -29,17 +29,6 @@ void TerminateService(void)
 
 DWORD ServiceBackupDaemon::WinService(void)
 {
-	WSADATA info;
-	
-	// Under Win32 we must initialise the Winsock library
-	// before using it.
-	
-	if (WSAStartup(MAKELONG(1, 1), &info) == SOCKET_ERROR) 
-	{
-		// throw error?    perhaps give it its own id in the furture
-		THROW_EXCEPTION(BackupStoreException, Internal)
-	}
-
 	int argc = 2;
 	//first off get the path name for the default 
 	char buf[MAX_PATH];
@@ -53,10 +42,8 @@ DWORD ServiceBackupDaemon::WinService(void)
 	const char *argv[] = {conf.c_str(), cfile.c_str()};
 
 	MAINHELPER_START
-	return this->Main(BOX_FILE_BBACKUPD_DEFAULT_CONFIG, argc, argv);
 
-	// Clean up our sockets
-	WSACleanup();
+	return this->Main(BOX_FILE_BBACKUPD_DEFAULT_CONFIG, argc, argv);
 
 	MAINHELPER_END
 }
