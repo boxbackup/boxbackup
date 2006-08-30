004e6057759b08cd05c7f7a8599f43d6bc7d01c0
chris <chris@9c52e268-1103-0410-960d-c6a436f6b474>
2006-08-30 18:28:29 +0000

    * bin/bbstored/BackupStoreDaemon.h - Revert to trunk
    git-svn-id: https://www.boxbackup.org/svn/box/chris/merge@828 9c52e268-1103-0410-960d-c6a436f6b474

--- bin/bbstored/BackupStoreDaemon.h
+++ bin/bbstored/BackupStoreDaemon.h
@@ -38,13 +38,11 @@ private:
 	BackupStoreDaemon(const BackupStoreDaemon &rToCopy);
 public:
 
-#ifndef WIN32
 	// For BackupContext to comminicate with housekeeping process
 	void SendMessageToHousekeepingProcess(const void *Msg, int MsgLen)
 	{
 		mInterProcessCommsSocket.Write(Msg, MsgLen);
 	}
-#endif
 
 protected:
 	
@@ -59,11 +57,9 @@ protected:
 
 	const ConfigurationVerify *GetConfigVerify() const;
 	
-#ifndef WIN32	
 	// Housekeeping functions
 	void HousekeepingProcess();
 	bool CheckForInterProcessMsg(int AccountNum = 0, int MaximumWaitTime = 0);
-#endif
 
 	void LogConnectionStats(const char *commonName, const SocketStreamTLS &s);
 
@@ -74,17 +70,8 @@ private:
 	bool mHaveForkedHousekeeping;
 	bool mIsHousekeepingProcess;
 	
-#ifdef WIN32
-	virtual void OnIdle();
-	bool mHousekeepingInited;
-#else
 	SocketStream mInterProcessCommsSocket;
 	IOStreamGetLine mInterProcessComms;
-#endif
-
-	void HousekeepingInit();
-	void RunHousekeepingIfNeeded();
-	int64_t mLastHousekeepingRun;
 };
 
 
