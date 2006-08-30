990662e08a11d240d65ec2ebdd450322bbbd2285
chris <chris@9c52e268-1103-0410-960d-c6a436f6b474>
2006-08-30 18:20:08 +0000

    * bin/bbstored/BackupContext.cpp - Revert to trunk
    git-svn-id: https://www.boxbackup.org/svn/box/chris/merge@824 9c52e268-1103-0410-960d-c6a436f6b474

--- bin/bbstored/BackupContext.cpp
+++ bin/bbstored/BackupContext.cpp
@@ -132,7 +132,6 @@ bool BackupContext::AttemptToGetWriteLock()
 	// Request the lock
 	bool gotLock = mWriteLock.TryAndGetLock(writeLockFile.c_str(), 0600 /* restrictive file permissions */);
 	
-#ifndef WIN32
 	if(!gotLock)
 	{
 		// The housekeeping process might have the thing open -- ask it to stop
@@ -151,7 +150,6 @@ bool BackupContext::AttemptToGetWriteLock()
 			
 		} while(!gotLock && tries > 0);
 	}
-#endif
 	
 	if(gotLock)
 	{
@@ -455,21 +453,13 @@ int64_t BackupContext::AddFile(IOStream &rFile, int64_t InDirectory, int64_t Mod
 			try
 			{
 				// Open it twice
-#ifdef WIN32
-				FileStream diff(tempFn.c_str(), 
-					O_RDWR | O_CREAT | O_BINARY);
-				FileStream diff2(tempFn.c_str(), 
-					O_RDWR | O_BINARY);
-#else
 				FileStream diff(tempFn.c_str(), O_RDWR | O_CREAT | O_EXCL);
 				FileStream diff2(tempFn.c_str(), O_RDONLY);
-
-				// Unlink it immediately, so it definitely goes away
+				// Unlink it immediately, so it definately goes away
 				if(::unlink(tempFn.c_str()) != 0)
 				{
 					THROW_EXCEPTION(CommonException, OSFileError);
 				}
-#endif
 				
 				// Stream the incoming diff to this temporary file
 				if(!rFile.CopyStreamTo(diff, BACKUP_STORE_TIMEOUT))
@@ -518,14 +508,6 @@ int64_t BackupContext::AddFile(IOStream &rFile, int64_t InDirectory, int64_t Mod
 				::unlink(tempFn.c_str());
 				throw;
 			}
-
-#ifdef WIN32
-			// we can't delete the file while it's open, above
-			if(::unlink(tempFn.c_str()) != 0)
-			{
-				THROW_EXCEPTION(CommonException, OSFileError);
-			}
-#endif
 		}
 		
 		// Get the blocks used
