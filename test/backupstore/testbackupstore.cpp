3e276847db37d46b33a96cf8c0ba9f58af016f00
Chris Wilson <chris+github@qwirx.com>
2006-10-27 18:53:20 +0000

    Disable symlink tests on Win32

--- test/backupstore/testbackupstore.cpp
+++ test/backupstore/testbackupstore.cpp
@@ -1611,6 +1611,7 @@ int test3(int argc, const char *argv[])
 			TEST_THAT(decoded->GetNumBlocks() == 3);
 		}
 		
+#ifndef WIN32 // no symlinks on Win32
 		// Try out doing this on a symlink
 		{
 			TEST_THAT(::symlink("does/not/exist", "testfiles/testsymlink") == 0);
@@ -1624,6 +1625,7 @@ int test3(int argc, const char *argv[])
 			// Decode it
 			BackupStoreFile::DecodeFile(b, "testfiles/testsymlink_2", IOStream::TimeOutInfinite);
 		}
+#endif
 	}
 
 	// Store info
