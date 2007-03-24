bf4d64174cdfd6953fe597dba016179b523a64eb
chris <chris@9c52e268-1103-0410-960d-c6a436f6b474>
2007-03-24 23:41:48 +0000

    Initialise logging framework and set our program name to "Box Backup (bbstored)". (refs #3, merges [1462])
    git-svn-id: https://www.boxbackup.org/svn/box/chris/merge@1488 9c52e268-1103-0410-960d-c6a436f6b474

--- bin/bbstored/bbstored.cpp
+++ bin/bbstored/bbstored.cpp
@@ -10,12 +10,17 @@
 #include "Box.h"
 #include "BackupStoreDaemon.h"
 #include "MainHelper.h"
+#include "Logging.h"
 
 #include "MemLeakFindOn.h"
 
 int main(int argc, const char *argv[])
 {
 	MAINHELPER_START
+
+	Logging::SetProgramName("Box Backup (bbstored)");
+	Logging::ToConsole(true);
+	Logging::ToSyslog (true);
 
 	BackupStoreDaemon daemon;
 	return daemon.Main(BOX_FILE_BBSTORED_DEFAULT_CONFIG, argc, argv);
