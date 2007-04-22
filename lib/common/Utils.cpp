a735816fafd65432559b7be3730d595955223ab7
chris <chris@9c52e268-1103-0410-960d-c6a436f6b474>
2007-04-22 14:07:11 +0000

    Use logging framework to log stack traces from exceptions. (refs #3)
    git-svn-id: https://www.boxbackup.org/svn/box/chris/merge@1549 9c52e268-1103-0410-960d-c6a436f6b474

--- lib/common/Utils.cpp
+++ lib/common/Utils.cpp
@@ -20,6 +20,7 @@
 
 #include "Utils.h"
 #include "CommonException.h"
+#include "Logging.h"
 
 #include "MemLeakFindOn.h"
 
@@ -71,11 +72,11 @@ void DumpStackBacktrace()
 	size = backtrace (array, 10);
 	strings = backtrace_symbols (array, size);
 
-	printf ("Obtained %zd stack frames.\n", size);
+	BOX_TRACE("Obtained " << size << " stack frames.");
 
 	for(i = 0; i < size; i++)
 	{
-		printf("%s\n", strings[i]);
+		BOX_TRACE(strings[i]);
 	}
 
 #include "MemLeakFindOff.h"
