3fb02f866aab7fb0fae89df384b807bffe2a686c
Chris Wilson <chris+github@qwirx.com>
2007-07-26 20:56:20 +0000

    Typo compile fix.

--- lib/common/Box.h
+++ lib/common/Box.h
@@ -112,7 +112,7 @@
 #define THROW_EXCEPTION(type, subtype) \
 	{ \
 		OPTIONAL_DO_BACKTRACE \
-		BOX_WARN("Exception thrown: " #type "(" #subtype ") at " \
+		BOX_WARNING("Exception thrown: " #type "(" #subtype ") at " \
 			__FILE__ "(" << __LINE__ << ")") \
 		throw type(type::subtype); \
 	}
