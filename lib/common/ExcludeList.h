8bc9c724d51312906be6e965b45a7bc071cfb931
Chris Wilson <chris+github@qwirx.com>
2007-07-26 22:05:56 +0000

    Work around the fact that we may have regex support without having regex.h (e.g. from pcreposix.h/libpcreposix) and disabuse HAVE_REGEX_H, define and use HAVE_REGEX_SUPPORT instead, thanks Gary! (refs #3, merges [1677] [1678] [1679])

--- lib/common/ExcludeList.h
+++ lib/common/ExcludeList.h
@@ -50,7 +50,7 @@ public:
 	// Mainly for tests
 	unsigned int SizeOfDefiniteList() const {return mDefinite.size();}
 	unsigned int SizeOfRegexList() const
-#ifdef HAVE_REGEX_H
+#ifdef HAVE_REGEX_SUPPORT
 		{return mRegex.size();}
 #else
 		{return 0;}
@@ -58,7 +58,7 @@ public:
 
 private:
 	std::set<std::string> mDefinite;
-#ifdef HAVE_REGEX_H
+#ifdef HAVE_REGEX_SUPPORT
 	std::vector<regex_t *> mRegex;
 	std::vector<std::string> mRegexStr;	// save original regular expression string-based source for Serialize
 #endif
