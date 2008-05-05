7eba897f6da8b1a993f0d40251af1748b5c793ea
Chris Wilson <chris+github@qwirx.com>
2008-05-05 14:44:38 +0000

    Add restore -f option to force restore to continue after an error.

--- lib/backupclient/BackupClientRestore.h
+++ lib/backupclient/BackupClientRestore.h
@@ -15,14 +15,21 @@ class BackupProtocolClient;
 enum
 {
 	Restore_Complete = 0,
-	Restore_ResumePossible = 1,
-	Restore_TargetExists = 2,
-	Restore_TargetPathNotFound = 3,
-	Restore_UnknownError = 4,
+	Restore_ResumePossible,
+	Restore_TargetExists,
+	Restore_TargetPathNotFound,
+	Restore_UnknownError,
+	Restore_CompleteWithErrors,
 };
 
-int BackupClientRestore(BackupProtocolClient &rConnection, int64_t DirectoryID, const char *LocalDirectoryName,
-	bool PrintDots = false, bool RestoreDeleted = false, bool UndeleteAfterRestoreDeleted = false, bool Resume = false);
+int BackupClientRestore(BackupProtocolClient &rConnection,
+	int64_t DirectoryID,
+	const char *LocalDirectoryName,
+	bool PrintDots = false,
+	bool RestoreDeleted = false,
+	bool UndeleteAfterRestoreDeleted = false,
+	bool Resume = false,
+	bool ContinueAfterErrors = false);
 
 #endif // BACKUPSCLIENTRESTORE__H
 
