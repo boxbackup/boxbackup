ab98fc40bc7ebe13f0def0dd3055afe53e82e137
chris <chris@9c52e268-1103-0410-960d-c6a436f6b474>
2008-05-28 14:40:39 +0000

    Shorten names used by daemons.
    git-svn-id: https://www.boxbackup.org/svn/box/trunk@2180 9c52e268-1103-0410-960d-c6a436f6b474

--- bin/bbackupd/bbackupd.cpp
+++ bin/bbackupd/bbackupd.cpp
@@ -29,7 +29,7 @@ int main(int argc, const char *argv[])
 
 	MAINHELPER_START
 
-	Logging::SetProgramName("Box Backup (bbackupd)");
+	Logging::SetProgramName("bbackupd");
 	Logging::ToConsole(true);
 	Logging::ToSyslog (true);
 	
