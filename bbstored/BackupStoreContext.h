c698798b1f462af2297fd18594ecb64ee840c759
Chris Wilson <chris+github@qwirx.com>
2008-09-13 15:29:26 +0000

    Add command to undelete a file, to complete the set of commands implemented by the bbstored server.

--- bbstored/BackupStoreContext.h
+++ bbstored/BackupStoreContext.h
@@ -112,6 +112,7 @@ public:
 	void ChangeDirAttributes(int64_t Directory, const StreamableMemBlock &Attributes, int64_t AttributesModTime);
 	bool ChangeFileAttributes(const BackupStoreFilename &rFilename, int64_t InDirectory, const StreamableMemBlock &Attributes, int64_t AttributesHash, int64_t &rObjectIDOut);
 	bool DeleteFile(const BackupStoreFilename &rFilename, int64_t InDirectory, int64_t &rObjectIDOut);
+	bool UndeleteFile(int64_t ObjectID, int64_t InDirectory);
 	void DeleteDirectory(int64_t ObjectID, bool Undelete = false);
 	void MoveObject(int64_t ObjectID, int64_t MoveFromDirectory, int64_t MoveToDirectory, const BackupStoreFilename &rNewFilename, bool MoveAllWithSameName, bool AllowMoveOverDeletedObject);
 
