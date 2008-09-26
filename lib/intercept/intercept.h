4bb33be645a37da5af0f27b3593985148e366388
Chris Wilson <chris+github@qwirx.com>
2008-09-26 20:24:11 +0000

    Fix compilation of open64() intercept.

--- lib/intercept/intercept.h
+++ lib/intercept/intercept.h
@@ -23,14 +23,17 @@ extern "C"
 	typedef struct dirent *(readdir_t) (DIR *dir);
 	typedef int            (closedir_t)(DIR *dir);
 #if defined __GNUC__ && __GNUC__ >= 2
-#define LINUX_WEIRD_LSTAT
-#define STAT_STRUCT struct stat /* should be stat64 */
-	typedef int            (lstat_t)   (int ver, const char *file_name, 
-	                                    STAT_STRUCT *buf);
+	#if _FILE_OFFSET_BITS == 64
+		#define DEFINE_ONLY_OPEN64
+	#endif
+	#define LINUX_WEIRD_LSTAT
+	#define STAT_STRUCT struct stat /* should be stat64 */
+		typedef int    (lstat_t)   (int ver, const char *file_name, 
+					    STAT_STRUCT *buf);
 #else
-#define STAT_STRUCT struct stat
-	typedef int            (lstat_t)   (const char *file_name, 
-	                                    STAT_STRUCT *buf);
+	#define STAT_STRUCT struct stat
+		typedef int    (lstat_t)   (const char *file_name, 
+					    STAT_STRUCT *buf);
 #endif
 }
 
