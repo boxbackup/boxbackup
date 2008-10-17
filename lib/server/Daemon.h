4de73ee45c4a36c22a383e7c2781589447c566e9
Chris Wilson <chris+github@qwirx.com>
2008-10-17 20:26:01 +0000

    Fix compile errors on Debian Lenny, reported by Torsten.

--- lib/server/Daemon.h
+++ lib/server/Daemon.h
@@ -19,8 +19,8 @@
 #include <string>
 
 #include "BoxTime.h"
+#include "Configuration.h"
 
-class Configuration;
 class ConfigurationVerify;
 
 // --------------------------------------------------------------------------
