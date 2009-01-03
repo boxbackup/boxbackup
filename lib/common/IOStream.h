2b1c6f82d57d187a20e4a7ebb34f3abbc22bac71
Chris Wilson <chris+github@qwirx.com>
2009-01-03 08:57:53 +0000

    Add a method to write a C style string directly to an IOStream.

--- lib/common/IOStream.h
+++ lib/common/IOStream.h
@@ -44,6 +44,10 @@ public:
 	virtual int Read(void *pBuffer, int NBytes, int Timeout = IOStream::TimeOutInfinite) = 0;
 	virtual pos_type BytesLeftToRead();	// may return IOStream::SizeOfStreamUnknown (and will for most stream types)
 	virtual void Write(const void *pBuffer, int NBytes) = 0;
+	virtual void Write(const char *pBuffer)
+	{
+		Write(pBuffer, strlen(pBuffer));
+	}
 	virtual void WriteAllBuffered();
 	virtual pos_type GetPosition() const;
 	virtual void Seek(pos_type Offset, int SeekType);
