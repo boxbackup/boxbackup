29820f220b9a7c220310e34957805864c9b7c7c6
chris <chris@9c52e268-1103-0410-960d-c6a436f6b474>
2010-02-16 22:12:18 +0000

    Move modification time functions out-of-line to help with debugging them.
    git-svn-id: https://www.boxbackup.org/svn/box/trunk@2612 9c52e268-1103-0410-960d-c6a436f6b474

--- lib/common/FileModificationTime.h
+++ lib/common/FileModificationTime.h
@@ -14,51 +14,9 @@
 
 #include "BoxTime.h"
 
-inline box_time_t FileModificationTime(EMU_STRUCT_STAT &st)
-{
-#ifndef HAVE_STRUCT_STAT_ST_MTIMESPEC
-	box_time_t datamodified = ((int64_t)st.st_mtime) * (MICRO_SEC_IN_SEC_LL);
-#else
-	box_time_t datamodified = (((int64_t)st.st_mtimespec.tv_nsec) / NANO_SEC_IN_USEC_LL)
-			+ (((int64_t)st.st_mtimespec.tv_sec) * (MICRO_SEC_IN_SEC_LL));
-#endif
-	
-	return datamodified;
-}
-
-inline box_time_t FileAttrModificationTime(EMU_STRUCT_STAT &st)
-{
-	box_time_t statusmodified =
-#ifdef HAVE_STRUCT_STAT_ST_MTIMESPEC
-		(((int64_t)st.st_ctimespec.tv_nsec) / (NANO_SEC_IN_USEC_LL)) +
-		(((int64_t)st.st_ctimespec.tv_sec)  * (MICRO_SEC_IN_SEC_LL));
-#elif defined HAVE_STRUCT_STAT_ST_ATIM_TV_NSEC
-		(((int64_t)st.st_ctim.tv_nsec) / (NANO_SEC_IN_USEC_LL)) +
-		(((int64_t)st.st_ctim.tv_sec)  * (MICRO_SEC_IN_SEC_LL));
-#elif defined HAVE_STRUCT_STAT_ST_ATIMENSEC
-		(((int64_t)st.st_ctimensec) / (NANO_SEC_IN_USEC_LL)) +
-		(((int64_t)st.st_ctime)     * (MICRO_SEC_IN_SEC_LL));
-#else // no nanoseconds anywhere
-		(((int64_t)st.st_ctime) * (MICRO_SEC_IN_SEC_LL));
-#endif
-	
-	return statusmodified;
-}
-
-inline box_time_t FileModificationTimeMaxModAndAttr(EMU_STRUCT_STAT &st)
-{
-#ifndef HAVE_STRUCT_STAT_ST_MTIMESPEC
-	box_time_t datamodified = ((int64_t)st.st_mtime) * (MICRO_SEC_IN_SEC_LL);
-	box_time_t statusmodified = ((int64_t)st.st_ctime) * (MICRO_SEC_IN_SEC_LL);
-#else
-	box_time_t datamodified = (((int64_t)st.st_mtimespec.tv_nsec) / NANO_SEC_IN_USEC_LL)
-			+ (((int64_t)st.st_mtimespec.tv_sec) * (MICRO_SEC_IN_SEC_LL));
-	box_time_t statusmodified = (((int64_t)st.st_ctimespec.tv_nsec) / NANO_SEC_IN_USEC_LL)
-			+ (((int64_t)st.st_ctimespec.tv_sec) * (MICRO_SEC_IN_SEC_LL));
-#endif
-	
-	return (datamodified > statusmodified)?datamodified:statusmodified;
-}
+box_time_t FileModificationTime(EMU_STRUCT_STAT &st);
+box_time_t FileAttrModificationTime(EMU_STRUCT_STAT &st);
+box_time_t FileModificationTimeMaxModAndAttr(EMU_STRUCT_STAT &st);
 
 #endif // FILEMODIFICATIONTIME__H
 
