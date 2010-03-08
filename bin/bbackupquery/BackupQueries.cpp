52914c342f4a2428352418ffef38ccd83603538f
Chris Wilson <chris+github@qwirx.com>
2010-03-08 21:35:19 +0000

    Merge [2601] [2602] [2603] from trunk into 0.11rc7. Don't try to stat excluded directories during compare, reported by Ben Bennett.

--- bin/bbackupquery/BackupQueries.cpp
+++ bin/bbackupquery/BackupQueries.cpp
@@ -1544,13 +1544,29 @@ void BackupQueries::Compare(int64_t DirID, const std::string &rStoreDir,
 				continue;
 			}
 
-#ifndef HAVE_VALID_DIRENT_D_TYPE
-			std::string fn(MakeFullPath
-				(rLocalDir, localDirEn->d_name));
-			EMU_STRUCT_STAT st;
-			if(EMU_LSTAT(fn.c_str(), &st) != 0)
+			std::string localDirPath(MakeFullPath(rLocalDir,
+				localDirEn->d_name));
+			std::string storeDirPath(rStoreDir + "/" +
+				localDirEn->d_name);
+
+			// Check whether dir is excluded before trying to
+			// stat it, to fix problems with .gvfs directories
+			// that are not readable by root causing compare
+			// to crash:
+			// http://lists.boxbackup.org/pipermail/boxbackup/2010-January/000013.html
+			if(rParams.IsExcludedDir(localDirPath))
 			{
-			    THROW_EXCEPTION(CommonException, OSFileError)
+				rParams.NotifyExcludedDir(localDirPath,
+					storeDirPath);
+				continue;
+			}
+
+#ifndef HAVE_VALID_DIRENT_D_TYPE
+			EMU_STRUCT_STAT st;
+			if(EMU_LSTAT(localDirPath.c_str(), &st) != 0)
+			{
+			    THROW_EXCEPTION_MESSAGE(CommonException,
+				OSFileError, localDirPath);
 			}
 			
 			// Entry -- file or dir?
