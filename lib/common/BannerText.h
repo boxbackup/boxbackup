b63f27c0923a6758e5e5dbe2bc42e06a93049443
Chris Wilson <chris+github@qwirx.com>
2011-10-25 22:03:07 +0000

    Avoid recompiling everything on MSVC when BoxVersion.h changes.

--- lib/common/BannerText.h
+++ lib/common/BannerText.h
@@ -10,9 +10,13 @@
 #ifndef BANNERTEXT__H
 #define BANNERTEXT__H
 
+#ifdef NEED_BOX_VERSION_H
+#	include "BoxVersion.h"
+#endif
+
 #define BANNER_TEXT(UtilityName) \
 	"Box " UtilityName " v" BOX_VERSION ", (c) Ben Summers and " \
-	"contributors 2003-2010"
+	"contributors 2003-2011"
 
 #endif // BANNERTEXT__H
 
