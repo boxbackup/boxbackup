e73c8f1836b13f4dfeef2ad64a66679a6a862f83
Chris Wilson <chris+github@qwirx.com>
2014-02-07 21:17:10 +0000

    Allow InvisibleTempFileStream to take a std::string.
    For C++ style.

--- lib/common/InvisibleTempFileStream.cpp
+++ lib/common/InvisibleTempFileStream.cpp
@@ -22,7 +22,8 @@
 //		Created: 2006/10/13
 //
 // --------------------------------------------------------------------------
-InvisibleTempFileStream::InvisibleTempFileStream(const char *Filename, int flags, int mode)
+InvisibleTempFileStream::InvisibleTempFileStream(const std::string& Filename,
+	int flags, int mode)
 #ifdef WIN32
 	: FileStream(Filename, flags | O_TEMPORARY, mode)
 #else
@@ -30,7 +31,7 @@ InvisibleTempFileStream::InvisibleTempFileStream(const char *Filename, int flags
 #endif
 {
 	#ifndef WIN32
-	if(unlink(Filename) != 0)
+	if(unlink(Filename.c_str()) != 0)
 	{
 		MEMLEAKFINDER_NOT_A_LEAK(this);
 		THROW_EXCEPTION(CommonException, OSFileOpenError)
