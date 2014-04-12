5d168d9eeb12ce2a351b40f0ac1ec624dc97cfda
chris <chris@9c52e268-1103-0410-960d-c6a436f6b474>
2014-04-12 11:47:12 +0000

    Add missing BackgroundTask header
    git-svn-id: https://www.boxbackup.org/svn/box/trunk@3326 9c52e268-1103-0410-960d-c6a436f6b474

---
+++ lib/backupstore/BackgroundTask.h
@@ -0,0 +1,39 @@
+// --------------------------------------------------------------------------
+//
+// File
+//		Name:    BackgroundTask.h
+//		Purpose: Declares the BackgroundTask interface.
+//		Created: 2014/04/07
+//
+// --------------------------------------------------------------------------
+
+#ifndef BACKGROUNDTASK__H
+#define BACKGROUNDTASK__H
+
+// --------------------------------------------------------------------------
+//
+// Class
+//		Name:    BackgroundTask
+//		Purpose: Provides a RunBackgroundTask() method which allows
+//			 background tasks such as polling the command socket
+//			 to happen while a file is being uploaded. If it
+//			 returns false, the current task should be aborted.
+//		Created: 2014/04/07
+//
+// --------------------------------------------------------------------------
+class BackgroundTask
+{
+	public:
+	enum State {
+		Unknown = 0,
+		Scanning_Dirs,
+		Searching_Blocks,
+		Uploading_Full,
+		Uploading_Patch,
+	};
+	virtual ~BackgroundTask() { }
+	virtual bool RunBackgroundTask(State state, uint64_t progress,
+		uint64_t maximum) = 0;
+};
+
+#endif // BACKGROUNDTASK__H
