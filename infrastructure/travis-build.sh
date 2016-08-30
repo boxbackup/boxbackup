0ebfa6464b9987b5bd2a084872f7511625647391
Chris Wilson <chris+github@qwirx.com>
2016-08-30 22:44:36 +0100

    Merge makebuildenv.pl and makeparcels.pl, write monolithic Makefiles.
    This is necessary to fix compilation on machines with large numbers of cores
    (e.g. Travis build workers) because the build system automatically attempts
    maximum parallelism, and the old one is broken and tries to build multiple
    libraries simultaneously.
    
    The new buildsystem almost entirely avoids recursive Make: only trivial
    Makefiles in target directories (which invoke the top-level Makefile to build
    the appropriate output file) and the old docs makefile are not part of the
    new master Makefile.
    
    Due to higher parallelism and better dependency checking, Make should be much
    faster now. Not all old targets are supported though.
    
    I've removed implicit dependencies hard-coded in the build system, and
    expressed all dependencies in modules.txt, since the implicit dependencies
    were confusing, obscure and unnecessary, and it was hard to express their
    own dependencies on each other.
    
    Test both the default target and explicitly "parcels" for release builds on Travis.

--- infrastructure/travis-build.sh
+++ infrastructure/travis-build.sh
@@ -11,5 +11,11 @@ cd `dirname $0`/..
 grep CXX config.status
 make V=1
 ./runtest.pl ALL $TEST_TARGET
+# When making a release build, also check that we can build the default
+# target and "parcels" (which is the same thing):
+if [ "$TEST_TARGET" = "release" ]; then
+	make
+	make parcels
+fi
 
 ccache -s
