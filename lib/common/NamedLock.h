c39226efb21c233c1f8362e3ea942e2078ff658b
Chris Wilson <chris+github@qwirx.com>
2017-04-22 16:47:08 +0100

    Improve NamedLock code readability, reliability and logging
    Define our own preprocessor defines for lock methods, test that we cover every
    case at compile time ad that they have the semantics that we expect (reentrant
    or not).
    
    (cherry picked from commit de1911b132310a1d5f1073928036db00abe0ef20)
    (cherry picked from commit 96136adf5dbe38ef92ad3f9490b549c11dd91fd4)

--- lib/common/NamedLock.h
+++ lib/common/NamedLock.h
@@ -35,7 +35,31 @@ public:
 private:
 	tOSFileHandle mFileDescriptor;
 	std::string mFileName;
+
+	typedef enum
+	{
+		LOCKTYPE_O_EXLOCK = 1,
+		LOCKTYPE_WIN32 = 2,
+		LOCKTYPE_F_SETLK = 3,
+		LOCKTYPE_FLOCK = 4,
+		LOCKTYPE_DUMB = 5,
+	}
+	LockType;
 };
+
+#if HAVE_DECL_O_EXLOCK
+#	define BOX_LOCK_TYPE_O_EXLOCK
+#elif defined BOX_OPEN_LOCK
+#	define BOX_LOCK_TYPE_WIN32
+#elif HAVE_DECL_F_SETLK
+#	define BOX_LOCK_TYPE_F_SETLK
+#elif defined HAVE_FLOCK
+#	define BOX_LOCK_TYPE_FLOCK
+#else
+// We have no other way to get a lock, so all we can do is fail if the
+// file already exists, and take the risk of stale locks.
+#	define BOX_LOCK_TYPE_DUMB
+#endif
 
 #endif // NAMEDLOCK__H
 
