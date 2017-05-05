98156a0d5dfd3f9135b79ce252f17021790988d7
Chris Wilson <chris+github@qwirx.com>
2017-05-05 20:59:08 +0100

    Hopefully fix CMake "make install" to not hit /usr/local on Travis and die

--- infrastructure/travis-build.sh
+++ infrastructure/travis-build.sh
@@ -28,7 +28,13 @@ if [ "$BUILD" = 'cmake' ]; then
 	cd cmake/build
 	cmake --version
 	cmake -DCMAKE_BUILD_TYPE:STRING=$TEST_TARGET $EXTRA_ARGS "$@" ..
-	make install
+
+	# We need to "make install" to install binaries in the boxbackup release/debug directories
+	# before we can run the tests. We don't particularly care about installing "for real" in
+	# /usr/local, and we don't have permission to do so anyway, but there's no way to prevent
+	# it, so redirect these files to /tmp instead:
+	make install DESTDIR=/tmp
+
 	[ "$TEST" = "n" ] || ctest -C $TEST_TARGET -V
 else
 	if [ "$TRAVIS_OS_NAME" = "osx" ]; then
