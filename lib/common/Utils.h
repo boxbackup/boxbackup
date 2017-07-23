b19bbca91e08274f87c6c4812c68e85488bb6569
Chris Wilson <chris+github@qwirx.com>
2017-07-23 21:51:27 +0100

    Refactor test code into process_is_running()
    We need the ability to check whether a process is still running to check for
    stale S3 account locks in SimpleDB, so move this code out of Test.cpp and into
    Utils.cpp, for use outside of tests.

--- lib/common/Utils.h
+++ lib/common/Utils.h
@@ -47,6 +47,8 @@ std::string FormatUsageBar(int64_t Blocks, int64_t Bytes, int64_t Max,
 std::string FormatUsageLineStart(const std::string& rName,
 	bool MachineReadable);
 
+bool process_is_running(int pid);
+
 #include "MemLeakFindOff.h"
 
 #endif // UTILS__H
