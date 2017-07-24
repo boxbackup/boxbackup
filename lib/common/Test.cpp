44ce225108a9e5c4d88c51a8ae76038f9f44690b
Chris Wilson <chris+github@qwirx.com>
2017-07-24 20:50:11 +0100

    Implement compare_lists and test_equal_maps in lib/common/Test.cpp

--- lib/common/Test.cpp
+++ lib/common/Test.cpp
@@ -635,3 +635,32 @@ std::auto_ptr<Configuration> load_config_file(const std::string& config_file,
 	return config;
 }
 
+bool test_equal_lists(const std::vector<std::string>& expected_items,
+	const std::vector<std::string>& actual_items)
+{
+	bool all_match = (expected_items.size() == actual_items.size());
+
+	for(size_t i = 0; i < std::max(expected_items.size(), actual_items.size()); i++)
+	{
+		const std::string& expected = (i < expected_items.size()) ? expected_items[i] : "None";
+		const std::string& actual   = (i < actual_items.size())   ? actual_items[i]   : "None";
+		TEST_EQUAL_LINE(expected, actual, "Item " << i);
+		all_match &= (expected == actual);
+	}
+
+	return all_match;
+}
+
+bool test_equal_maps(const str_map_t& expected_attrs, const str_map_t& actual_attrs)
+{
+	str_map_diff_t differences = compare_str_maps(expected_attrs, actual_attrs);
+	for(str_map_diff_t::iterator i = differences.begin(); i != differences.end(); i++)
+	{
+		const std::string& name = i->first;
+		const std::string& expected = i->second.first;
+		const std::string& actual = i->second.second;
+		TEST_EQUAL_LINE(expected, actual, "Wrong value for attribute " << name);
+	}
+
+	return differences.empty();
+}
