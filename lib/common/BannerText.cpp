9a9bd463b06faf1979584610d565d6d90e61b002
Chris Wilson <chris+github@qwirx.com>
2018-08-15 22:47:48 +0100

    AppVeyor: build with both MSBuild and NMake generators, to compare
    Add support for building with single-config generators (such as NMake)
    on Windows, to complement existing support for multi-config generators
    (GENERATOR_IS_MULTI_CONFIG).
    
    Log the compiler version and 32/64 bit build during compilation.
    
    Hopefully fix OpenSSL linking on Windows by forcing use of the correct
    libraries on AppVeyor (the ones that we built ourselves).
    
    Hopefully fix 32-bit and 64-bit environments on AppVeyor.

---
+++ lib/common/BannerText.cpp
@@ -0,0 +1,12 @@
+// --------------------------------------------------------------------------
+//
+// File
+//		Name:    BannerText.cpp
+//		Purpose: Banner text for daemons and utilities
+//		Created: 2018-08-16
+//
+// --------------------------------------------------------------------------
+
+#include "BannerText.h"
+
+#pragma message("Build signature: " BOX_BUILD_SIGNATURE)
